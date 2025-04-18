df4a4e3e646bf3d199530ea8e63bac07a57d63c5
chiendev256 <ng.luongchien356@gmail.com>
2025-04-19 01:50:44 +0700

    add new proj

---
+++ client/src/utils/constants.js
@@ -0,0 +1,18 @@
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                 Smart Contract                                 ||
+// ! ||--------------------------------------------------------------------------------||
+
+import abi from './upload.json'
+export const  contractAddress= '0x2e5b0dd1BCf94b968AD1F9b220342Ef3f5cb1190';
+export const contractAbi = abi;
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                   Pinata IPFS                                  ||
+// ! ||--------------------------------------------------------------------------------||
+
+export const API_Key =  'c852211ab7e25cb83515'
+export const API_Secret=  '4b6e7500bf49433b0b0abae64e436593a9998f6d60ffe9168ce5ac0d9f1e2871'
+export const JWT= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyMjE3NjI0NC0wYWFkLTQ4ZWYtOTAyNS1hNzg1NDE3MWQ1ZTMiLCJlbWFpbCI6Im5nLmx1b25nY2hpZW4zNTZAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImM4NTIyMTFhYjdlMjVjYjgzNTE1Iiwic2NvcGVkS2V5U2VjcmV0IjoiNGI2ZTc1MDBiZjQ5NDMzYjBiMGFiYWU2NGU0MzY1OTNhOTk5OGY2ZDYwZmZlOTE2OGNlNWFjMGQ5ZjFlMjg3MSIsImV4cCI6MTc3NjUzMTExMn0.8pHyMFfWOXCW8ARiKEVYu7i_qshxH81zsjUha4Kow94'
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/9KEc39jGMz3VrFc2shGn827Il4rSGs9V',
+      accounts: ['bdc96c89b44a9dcfecd339fad565f3b50e37b7a6bf16f48ab406ce1ab2f35c89'],
+    },}
+};
